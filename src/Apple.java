65831581ed51c6393f0447d8ebeeb2158dfa3bf8
fweigel <fweigel@united-internet.de>
2023-05-24 11:59:05 +0200

    initial commit

---
+++ src/Apple.java
@@ -0,0 +1,38 @@
+public class Apple extends BasicProperties {
+
+    private static final String boarderOutline = "\uD83D\uDFE9";
+
+
+    public static int score = 0;
+
+    public Apple() {
+        setEmoji("\uD83C\uDF4E");
+    }
+
+    public void addRandomApple(GameBoard screen, Apple apple) {
+        int width = (int) (Math.random() * (screen.getScreenWidth() - 1));
+        int height = (int) (Math.random() * (screen.getScreenHeight() - 1));
+        if (!screen.getObjectOnLocation(width, height).equals(boarderOutline))
+            screen.setObjectOnLocation(apple, width, height);
+        else addRandomApple(screen, apple);
+
+    }
+
+    public static boolean checkForApple(GameBoard screen) {
+        String[][] array = screen.returnScreenArray();
+        for (String[] strings : array) {
+            for (String string : strings) {
+                if (string.equals(boarderOutline)) {
+                    return true;
+                }
+            }
+        }
+        score++;
+        return false;
+    }
+
+    public static int getScore() {
+        return score;
+    }
+
+}
---
+++ src/Snake.java
@@ -0,0 +1,59 @@
+import java.util.Objects;
+
+public class Snake extends BasicProperties {
+
+
+    public Snake(GameBoard board) {
+        setEmoji("\uD83D\uDC0D");
+        setX((int) (Math.random() * (board.getScreenWidth() - 2)) + 1);
+        setY((int) (Math.random() * (board.getScreenHeight() - 2)) + 1);
+    }
+
+    public void moveSnake(GameBoard board, Snake snake, String direction) {
+        String boarderOutline = "\uD83D\uDFE9";
+        switch (direction) {
+            case "a" -> {
+                int newX = snake.getX() - 1;
+                if (newX >= 0 && !Objects.equals(board.getObjectOnLocation(newX, snake.getY()), boarderOutline)) {
+                    snake.setX(newX);
+                    board.setObjectOnLocation(snake, snake.getX(), snake.getY());
+                    board.ClearScreenLocation(snake.getX() + 1, snake.getY());
+                } else {
+                    GameOver.printScreen();
+                }
+            }
+            case "d" -> {
+                int newX = snake.getX() + 1;
+                if (newX < board.getScreenWidth() && !Objects.equals(board.getObjectOnLocation(newX, snake.getY()), boarderOutline)) {
+                    snake.setX(newX);
+                    board.setObjectOnLocation(snake, snake.getX(), snake.getY());
+                    board.ClearScreenLocation(snake.getX() - 1, snake.getY());
+                } else {
+                    GameOver.printScreen();
+                }
+            }
+            case "w" -> {
+                int newY = snake.getY() - 1;
+                if (newY >= 0 && !Objects.equals(board.getObjectOnLocation(snake.getX(), newY), boarderOutline)) {
+                    snake.setY(newY);
+                    board.setObjectOnLocation(snake, snake.getX(), snake.getY());
+                    board.ClearScreenLocation(snake.getX(), snake.getY() + 1);
+                } else {
+                    GameOver.printScreen();
+                }
+            }
+            case "s" -> {
+                int newY = snake.getY() + 1;
+                if (newY < board.getScreenHeight() && !Objects.equals(board.getObjectOnLocation(snake.getX(), newY), boarderOutline)) {
+                    snake.setY(newY);
+                    board.setObjectOnLocation(snake, snake.getX(), snake.getY());
+                    board.ClearScreenLocation(snake.getX(), snake.getY() - 1);
+                } else {
+                    GameOver.printScreen();
+                }
+            }
+            case "stop" -> System.exit(0);
+            default -> System.out.println("You can only enter w,a,s,d");
+        }
+    }
+}
